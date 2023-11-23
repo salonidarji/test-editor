c5b9631f514e813a1e05766be94e988f3d81f869
Saloni Darji <salonidarji3335@gmail.com>
2023-11-23 20:00:30 +0530

    demo test editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+
 
 function App() {
+  const data = [];
+  // [
+  //   {
+  //     section: {
+  //       name: "section name",
+  //       label: {
+  //         name: "label name",
+  //         question: {
+  //           name: "question",
+  //         },
+  //         or: true,
+  //       },
+  //     },
+  //   },
+  // ];
+
+  const [arr, setArr] = useState(data || []);
+  const [selected, setSelected] = useState(0);
+  const [labelSelected, setLabelSelected] = useState(0);
+
+  const addSection = () => {
+    setArr((prevArr) => [
+      ...prevArr,
+      { section: { name: "section name", label: [] } },
+    ]);
+  };
+
+  const addLabel = () => {
+    const temp = arr;
+    temp[selected].section.label.push({ name: "label name", question: [] });
+
+    temp[selected].section = { ...temp[selected].section };
+    setArr([...temp]);
+  };
+
+  const addQuestion = () => {
+    const temp = arr;
+    temp[selected].section.label[labelSelected]?.question.push({
+      name: "question",
+      or: false,
+    });
+    temp[selected].section = { ...temp[selected].section };
+    setArr([...temp]);
+  };
+
+  const addOr = () => {
+    const temp = arr;
+    temp[selected].section.label[labelSelected].or = true;
+    setArr([...temp]);
+  };
+
+  const handleDelete = (index) => {
+    setArr(arr.filter((obj,i) => index !== i))
+  }
+
+  useEffect(() => {
+    console.log("arr:", arr);
+  }, [arr]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container spacing={1}>
+        <Grid item xs={8}>
+          {arr.map((sectionObj, index) => {
+            return (
+              <>
+                <div>
+                  <FormControlLabel
+                    value={index}
+                    label={`${index + 1}. ${sectionObj.section.name}`}
+                    onChange={(e) => setSelected(e.target.value)}
+                    control={<Radio />}
+                  />
+                  <Button variant="text" color="error" onClick={()=>handleDelete(index)}>
+                    delete
+                  </Button>
+                </div>
+
+                {sectionObj.section?.label?.length > 0 &&
+                  sectionObj.section.label.map((ele, eleIndex) => (
+                    <>
+                      <div>
+                        <FormControlLabel
+                          value={eleIndex}
+                          label={`${eleIndex + 1}. ${ele?.name}`}
+                          onChange={(e) => setLabelSelected(e.target.value)}
+                          control={<Radio />}
+                        />
+                      </div>
+
+                      {ele?.question?.length > 0 &&
+                        ele.question.map((eleq, eleqIndex) => (
+                          <div>{`${eleqIndex + 1}. ${eleq?.name}`}</div>
+                        ))}
+                    </>
+                  ))}
+                <div>
+                  {sectionObj.section.label?.[labelSelected]?.or ? "or" : ""}
+                </div>
+              </>
+            );
+          })}
+        </Grid>
+        <Grid item xs={4}>
+          <Button variant="contained" color="primary" onClick={addSection}>
+            Section
+          </Button>
+
+          <Button variant="contained" color="warning" onClick={addLabel}>
+            Label
+          </Button>
+
+          <Button variant="contained" color="success" onClick={addQuestion}>
+            Question
+          </Button>
+
+          <Button variant="contained" color="inherit" onClick={addOr}>
+            OR
+          </Button>
+        </Grid>
+      </Grid>
     </div>
   );
 }
